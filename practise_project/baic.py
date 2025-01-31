015a23422c424496801df9acc8ae9b21abb1938e
Diksha Patidar <dikshapatidar32@gmail.com>
2025-01-31 18:14:22 +0530

    first commit

---
+++ practise_project/baic.py
@@ -0,0 +1,29 @@
+print("Hello")
+print("H")
+print("e")
+print("l")
+print("l")
+print("o")
+
+print("Hello", "Rajat","ho","are","You")
+print('******')
+print('******')
+print("******\n******\n******\n******\n")
+print("******\n"*5)
+print("my", "name", "i", "kritika" ,sep="-")
+print("my", "name", "i", "kritika" ,end="-")
+print(2)
+print(2.2)
+print("2")
+print(True)
+AGE =4
+print(AGE)
+print(type (AGE))
+age="four "
+print(age)
+print(type (age))
+name="Aditya"
+profession="Software developer"
+experience=10
+print("Hello, I am", name,". I am a",profession,"professionaly. And I have around",experience ,"with it.")
+print(type (name))
---
+++ practise_project/practice.py
@@ -0,0 +1,18 @@
+#print("Hello")
+'''
+this is sample
+comment for
+multi line
+'''
+print("Hello")
+print("Hello")
+print("Hello")
+age= 21
+#print(age)
+print("my age i:", 21)
+age="Five"
+print("my age i:",age)
+name= "Diksha"
+Profeion= "student"
+eperienced=0
+print("Hello,I Am ",name,". i am a ",Profeion," profeionaly. And i have around ",eperienced," year eperienced with it")
