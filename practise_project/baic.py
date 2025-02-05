9f693d18f417aa2aa59eff75d0d81fab6e76256b
Diksha Patidar <dikshapatidar32@gmail.com>
2025-02-05 18:16:36 +0530

    add comparison operator

--- practise_project/baic.py
+++ practise_project/baic.py
@@ -1,3 +1,4 @@
+"""
 print("Hello")
 print("H")
 print("e")
@@ -27,7 +28,9 @@ profession="Software developer"
 experience=10
 print("Hello, I am", name,". I am a",profession,"professionaly. And I have around",experience ,"with it.")
 print(type (name))
+"""
 
+"""
 # user input
 name=input("Enter your name:")
 print("Hello",str(name))
@@ -43,5 +46,16 @@ print("sum of two no is:",int (sum1)+int(sum2))
 sum1= input("Enter first no:")
 sum2= input("Enter Second no:")
 print("sum of two floating no:",float(sum1)+float(sum2))
-
-
+"""
+"""
+# Write a program to calculate  hypotenenus to sides
+a= input("Enter first side:")
+b = input("Enter second sides:")
+cal= ((float(a)**2) + (float(b)**2))**0.5
+print("hypotenuse of two sides is:", cal)
+"""
+# comparison Operator
+var=0
+print(var==0)
+var=1
+print(var==0)
