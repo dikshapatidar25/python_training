bfbed7228a2ec4712bd0ab9609013a25daa04cf7
Diksha Patidar <dikshapatidar32@gmail.com>
2025-03-13 19:27:20 +0530

    Add function and method

--- practise_project/practice.py
+++ practise_project/practice.py
@@ -3,6 +3,8 @@
 this is sample
 comment for
 multi line
+
+'''
 '''
 print("Hello")
 print("Hello")
@@ -28,3 +30,39 @@ number[len(number)//2]=var
 print(number)
 del number[len(number)-1]
 print(number)
+'''
+#FUNCTION
+list=[1,2,3,4,5]
+print("CUrrent list", list)
+list.append(6)
+print("updated list", list)
+list. insert(3,9)
+print("updated list after insert", list)
+
+# Traversing
+list=[1,2,3,4,5,6,7,8,9]
+for i in range (len(list)):
+    print(list[i])
+
+# Updating using for loop
+
+list=[]
+for count in range (10):
+    list.append(count+1)
+print(list)
+
+# write a program to update all elemnt of the list to add one to all element
+my_list = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
+for count in range(len(my_list)):
+    my_list[count] += 1
+print(my_list)
+
+# write a program to calculate the sum of the elemnet
+my_list = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
+sum=0
+for count in range(len(my_list)):
+    sum += my_list[count]
+    print(sum)
+
+
+
